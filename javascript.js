fd34d5c7ba5872c13baa7ceab9a998881ba7a9bb
clu23 <clement.lulurga@orange.fr>
2023-08-16 23:56:20 +0200

    Adding styling to the add book button and function showLibrary to the js file

--- javascript.js
+++ javascript.js
@@ -1,18 +1,49 @@
 let myLibrary=[];
 
-function Book(title, author, pages, read) {
-  this.title=title;
-  this.author=author;
-  this.pages=pages;
-  this.read=read;
-  // the constructor...
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 }
 
-function addBookToLibrary() {
-  // do stuff here
+function addBookToLibrary(title,author,pages,read) {
+  const book = new Book(title, author, pages, read);
+  myLibrary.push(book);
+  showBooksInLibrary();
 }
 
+function showLibraryInfo() {
+  const totalBooks = document.querySelector('#books_count');
+  const booksRead = document.querySelector('#read_b_count');
+  const booksUnread = document.querySelector('#not_read_b_count');
+  let readCounter = 0;
+  let unreadCounter = 0;
+  booksRead.textContent = 0;
+  booksUnread.textContent = 0;
+  for (let i = 0; i < myLibrary.length; i ++) {
+    if (myLibrary[i].status === true) {
+      readCounter += 1;
+      booksRead.textContent = readCounter;
+    } else if (myLibrary[i].status === false) {
+      unreadCounter += 1;
+      booksUnread.textContent = unreadCounter;
+    }
+  }
+  totalBooks.textContent = myLibrary.length;
+}
 
+function showBooksInLibrary(){
+  //do stuff here
+}
+
+function deleteAll(){
+  //delete all books from library
+  myLibrary=[]
+  showLibraryInfo()
+}
 
 
 
